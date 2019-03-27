48bbfc74cd6b68eedfa3e606d5137af2d34e0876
胡晴风 <1397649957@qq.com>
2019-03-27 14:07:27 +0800

    x

--- eureka/src/main/java/com/eureka/Eureka.java
+++ eureka/src/main/java/com/eureka/Eureka.java
@@ -3,8 +3,11 @@ package com.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -14,10 +17,12 @@ public class Eureka {
         SpringApplication.run(Eureka.class,args);
     }
 
+    @Resource
+    private ConfigurableEnvironment configurableEnvironment;
 
     @RequestMapping("/hello")
     public Object hello(){
-        return show();
+        return configurableEnvironment.getProperty("server.port");
     }
 
     public Object show(){
