6d42e72b89686b3d767923a5909ab230523bacd8
胡晴风 <1397649957@qq.com>
2019-04-27 21:50:26 +0800

    x

---
+++ alltest/src/main/java/com/excel/TestTomcat.java
@@ -0,0 +1,14 @@
+package com.excel;
+
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+
+/**
+ * @author huqingfeng
+ * @date 2019/4/27
+ */
+public class TestTomcat {
+    public static void main(String[] args) {
+        Tomcat tomcat = new Tomcat();
+    }
+}
