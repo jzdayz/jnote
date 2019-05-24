0701182bfe2acf0df71d92d40c872b891b973f46
胡晴风 <1397649957@qq.com>
2019-05-24 16:06:14 +0800

    提交一个文件

--- alltest/src/main/java/com/example/jnote/CustomSqlInjector.java
+++ alltest/src/main/java/com/example/jnote/CustomSqlInjector.java
@@ -4,14 +4,18 @@
 //import com.baomidou.mybatisplus.core.injector.AbstractSqlInjector;
 //import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
 //import com.baomidou.mybatisplus.core.metadata.TableInfo;
-//import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
-//import com.baomidou.mybatisplus.core.toolkit.GlobalConfigUtils;
-//import com.baomidou.mybatisplus.core.toolkit.TableInfoHelper;
+//import com.baomidou.mybatisplus.core.parser.SqlParserHelper;
+//import com.baomidou.mybatisplus.core.toolkit.*;
+//import com.pes.jd.mapper.BaseMapper;
 //import org.apache.ibatis.builder.MapperBuilderAssistant;
 //import org.apache.ibatis.logging.Log;
 //import org.apache.ibatis.logging.LogFactory;
 //import org.springframework.context.annotation.Configuration;
 //
+//import java.lang.reflect.ParameterizedType;
+//import java.lang.reflect.Type;
+//import java.lang.reflect.TypeVariable;
+//import java.lang.reflect.WildcardType;
 //import java.util.Arrays;
 //import java.util.List;
 //import java.util.Set;
@@ -23,32 +27,59 @@
 //
 //    private static final Class<?> customMapperClass = BaseMapper.class;
 //
+//    public static final String TABLE_NAME_PLACE_HOLDER = "${tableName}";
+//
 //    @Override
 //    public void inspectInject(MapperBuilderAssistant builderAssistant, Class<?> mapperClass) {
-//        Class<?> modelClass = extractModelClass(mapperClass);
-//        if (modelClass != null) {
-//            String className = mapperClass.toString();
-//            Set<String> mapperRegistryCache = GlobalConfigUtils.getMapperRegistryCache(builderAssistant.getConfiguration());
-//            if (!mapperRegistryCache.contains(className)) {
-//                List<AbstractMethod> methodList = this.getMethodList();
-//                if (CollectionUtils.isNotEmpty(methodList)) {
-////                    TableInfo tableInfo = TableInfoHelpear.initTableInfo(builderAssistant, modelClass);
-//                    final boolean custom = Arrays.asList(mapperClass.getInterfaces()).contains(customMapperClass);
-//                    String tableName=null;
-//                    if (custom){
-//                        tableName = tableInfo.getTableName();
-//                        tableInfo.setTableName("${tableName}");
-//                    }
-//                    // 循环注入自定义方法
-//                    methodList.forEach(m -> m.inject(builderAssistant, mapperClass, modelClass, tableInfo));
-//                    if (custom){
-//                        tableInfo.setTableName(tableName);
-//                    }
-//                } else {
-//                    logger.debug(mapperClass.toString() + ", No effective injection method was found.");
-//                }
-//                mapperRegistryCache.add(className);
+//        String className = mapperClass.toString();
+//        Set<String> mapperRegistryCache = GlobalConfigUtils.getMapperRegistryCache(builderAssistant.getConfiguration());
+//        if (!mapperRegistryCache.contains(className)) {
+//            List<AbstractMethod> methodList = this.getMethodList();
+//            Assert.notEmpty(methodList, "No effective injection method was found.");
+//            final boolean custom = Arrays.asList(mapperClass.getInterfaces()).contains(customMapperClass);
+//            String tableName=null;
+//            final Class<?> modelClass = extractModelClass(mapperClass);
+//            if (modelClass==null)return;
+//            TableInfo tableInfo = TableInfoHelper.initTableInfo(builderAssistant, modelClass);
+//            if (custom){
+//                tableName = tableInfo.getTableName();
+//                tableInfo.setTableName(TABLE_NAME_PLACE_HOLDER);
+//            }
+//            // 循环注入自定义方法
+//            methodList.forEach(m -> m.inject(builderAssistant, mapperClass));
+//            if (custom){
+//                tableInfo.setTableName(tableName);
+//            }
+//            mapperRegistryCache.add(className);
+//            /**
+//             * 初始化 SQL 解析
+//             */
+//            if (GlobalConfigUtils.getGlobalConfig(builderAssistant.getConfiguration()).isSqlParserCache()) {
+//                SqlParserHelper.initSqlParserInfoCache(mapperClass);
 //            }
 //        }
 //    }
+//
+//    private Class<?> extractModelClass(Class<?> mapperClass) {
+//        Type[] types = mapperClass.getGenericInterfaces();
+//        ParameterizedType target = null;
+//        for (Type type : types) {
+//            if (type instanceof ParameterizedType) {
+//                Type[] typeArray = ((ParameterizedType) type).getActualTypeArguments();
+//                if (ArrayUtils.isNotEmpty(typeArray)) {
+//                    for (Type t : typeArray) {
+//                        if (t instanceof TypeVariable || t instanceof WildcardType) {
+//                            break;
+//                        } else {
+//                            target = (ParameterizedType) type;
+//                            break;
+//                        }
+//                    }
+//                }
+//                break;
+//            }
+//        }
+//        return target == null ? null : (Class<?>) target.getActualTypeArguments()[0];
+//    }
+//
 //}
---
+++ alltest/src/main/java/com/example/jnote/DynamicTableParser.java
@@ -0,0 +1,8 @@
+package com.example.jnote;
+
+/**
+ * @author ☞ 🏀
+ * @date 2019-05-23
+ */
+public class DynamicTableParser {
+}
