62fe244560f73d8c42d6f6da71b997be9cc924bc
胡晴风 <1397649957@qq.com>
2019-05-31 21:45:08 +0800

    x

---
+++ alltest/src/main/java/com/excel/TimeUtils.java
@@ -0,0 +1,40 @@
+package com.excel;
+
+
+/**
+ * @author ☞ 🏀 huqingfeng
+ * @date 2019-05-30
+ */
+public class TimeUtils {
+
+    private static class Stopwatch{
+        private long mills;
+        public long watch(){
+            return System.currentTimeMillis() - mills;
+        }
+
+        public void reset() {
+            mills = System.currentTimeMillis();
+        }
+    }
+
+    private static final ThreadLocal<Stopwatch> stopwatchThreadLocal = ThreadLocal.withInitial(Stopwatch::new);
+
+    public static void reset(){
+        Stopwatch stopwatch = stopwatchThreadLocal.get();
+        stopwatch.reset();
+    }
+
+    public static long elapsed(){
+        return stopwatchThreadLocal.get().watch();
+    }
+
+    public static long elapsedSecends(){
+        return elapsed()/1000;
+    }
+
+    public static void clear(){
+        stopwatchThreadLocal.remove();
+    }
+
+}
---
+++ alltest/src/main/java/com/pes/jd/mapper/PesMenuResourceMapper.java
@@ -0,0 +1,30 @@
+package com.pes.jd.mapper;
+
+import com.pes.jd.model.DO.PesMenuResource;
+import com.pes.jd.model.DO.PesMenuResourceExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface PesMenuResourceMapper {
+    long countByExample(PesMenuResourceExample example);
+
+    int deleteByExample(PesMenuResourceExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(PesMenuResource record);
+
+    int insertSelective(PesMenuResource record);
+
+    List<PesMenuResource> selectByExample(PesMenuResourceExample example);
+
+    PesMenuResource selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") PesMenuResource record, @Param("example") PesMenuResourceExample example);
+
+    int updateByExample(@Param("record") PesMenuResource record, @Param("example") PesMenuResourceExample example);
+
+    int updateByPrimaryKeySelective(PesMenuResource record);
+
+    int updateByPrimaryKey(PesMenuResource record);
+}
