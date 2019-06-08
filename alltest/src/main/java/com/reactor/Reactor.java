d7b07bfbe0dc5316c68964f3622b8afa6add0feb
huqingfeng <1397649957@qq.com>
2019-06-08 10:42:06 +0800

    x

--- alltest/src/main/java/com/reactor/Reactor.java
+++ alltest/src/main/java/com/reactor/Reactor.java
@@ -17,31 +17,4 @@ public class Reactor {
     public static void main(String[] args) {
         Flux.just("1","2").concatMap(k->Flux.just(k.getClass(),"嘿嘿")).subscribe(System.out::println);
     }
-
-    @Test
-    public void show(){
-
-        System.out.println(Stream.of(1,2,3).reduce((x,y)->{
-            System.out.println(x+" "+y);
-            return x+y;
-        }).get());
-
-    }
-
-    @Test
-    public void show1(){
-
-        class b{
-            private boolean a;
-
-            public b(boolean a) {
-                this.a = a;
-            }
-        }
-        ArrayList<b> a = new ArrayList<>();
-        a.add(new b(true));
-        a.add(new b(false));
-        final List<b> collect = a.stream().filter(k -> k.a).collect(Collectors.toList());
-        System.out.println(a.get(0) == collect.get(0));
-    }
 }
