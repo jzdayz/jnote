7bfa3388f82356077c5e3aca2b67f630f7e5534a
huqingfeng <1397649957@qq.com>
2019-06-08 18:03:03 +0800

    x

---
+++ alltest/src/main/java/com/openfeign/TestOpenFeign.java
@@ -0,0 +1,30 @@
+package com.openfeign;
+
+import com.alibaba.fastjson.JSON;
+import feign.Feign;
+import feign.Headers;
+import feign.RequestLine;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
+import org.apache.commons.collections4.MultiMapUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestOpenFeign {
+
+    public static void main(String[] args) {
+        Remote target = Feign.builder().decoder(new JacksonDecoder()).encoder(new JacksonEncoder()).target(Remote.class, "http://httpbin.org");
+        Map<String,Object> body = new HashMap<>();
+        body.put("name","xioaming");
+        Object test = target.test(JSON.toJSONString(body));
+        System.out.println(String.format("%s \n type=%s",test,test.getClass()));
+    }
+
+    interface Remote{
+        @Headers("Content-Type: application/json")
+        @RequestLine("POST /post")
+        Object test(Object body);
+    }
+
+}
