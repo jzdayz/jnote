f73bbca2cfa52894a1a2198b84cf8e7b287489ea
huqingfeng <1397649957@qq.com>
2019-06-09 14:01:18 +0800

    x

---
+++ alltest/src/main/java/com/asm/TestParameterNames.java
@@ -0,0 +1,10 @@
+package com.asm;
+
+import java.util.List;
+
+public class TestParameterNames {
+    public static void main(String[] args) throws Exception{
+        List<String> main = ParameterNames.getParameterNames(TestParameterNames.class.getDeclaredMethod("main", String[].class));
+        System.out.println(main);
+    }
+}
---
+++ alltest/src/main/java/com/spring/framework/ValueTest.java
@@ -0,0 +1,25 @@
+package com.spring.framework;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+public class ValueTest {
+    public static void main(String[] args) {
+        SpringApplication sa = new SpringApplication(ValueTest.class);
+        sa.setApplicationContextClass(AnnotationConfigApplicationContext.class);
+        ConfigurableApplicationContext run = sa.run(args);
+        ConfigurableEnvironment environment = run.getEnvironment();
+        System.out.println(JSON.toJSONString(environment));
+    }
+
+
+    @Value("${file.separator}")
+    private String separator;
+
+}
