106feb98e677cd36eae0fa96b6a8ea1deaeaa098
胡晴风 <1397649957@qq.com>
2019-06-17 10:11:31 +0800

    x

---
+++ alltest/src/main/java/com/jdk/TestRemoveLinkedHashMap.java
@@ -0,0 +1,29 @@
+package com.jdk;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @author ☞ 🏀 huqingfeng
+ * @date 2019-06-17
+ */
+public class TestRemoveLinkedHashMap {
+    @Test
+    public void test1(){
+        LinkedHashMap linkedHashMap = new LinkedHashMap();
+        linkedHashMap.put("aa","bb");
+        linkedHashMap.put("cc","bb");
+        linkedHashMap.put("dd","bb");
+        Iterator iterator = linkedHashMap.entrySet().iterator();
+        for (;iterator.hasNext();){
+            Map.Entry next = (Map.Entry)iterator.next();
+            if (Objects.equals(next.getKey(),"cc")){
+                iterator.remove();
+            }
+        }
+        System.out.println(linkedHashMap);
+
+
+    }
+}
