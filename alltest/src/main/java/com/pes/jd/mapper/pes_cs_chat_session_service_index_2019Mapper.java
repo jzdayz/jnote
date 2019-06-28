2a14a31e814e4b285c0081d0e9b0fac32162ee18
胡晴风 <1397649957@qq.com>
2019-06-28 17:10:53 +0800

    x

---
+++ alltest/src/main/java/com/pes/jd/mapper/pes_cs_chat_session_service_index_2019Mapper.java
@@ -0,0 +1,30 @@
+package com.pes.jd.mapper;
+
+import com.pes.jd.model.DO.pes_cs_chat_session_service_index_2019;
+import com.pes.jd.model.DO.pes_cs_chat_session_service_index_2019Example;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface pes_cs_chat_session_service_index_2019Mapper {
+    long countByExample(pes_cs_chat_session_service_index_2019Example example);
+
+    int deleteByExample(pes_cs_chat_session_service_index_2019Example example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(pes_cs_chat_session_service_index_2019 record);
+
+    int insertSelective(pes_cs_chat_session_service_index_2019 record);
+
+    List<pes_cs_chat_session_service_index_2019> selectByExample(pes_cs_chat_session_service_index_2019Example example);
+
+    pes_cs_chat_session_service_index_2019 selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") pes_cs_chat_session_service_index_2019 record, @Param("example") pes_cs_chat_session_service_index_2019Example example);
+
+    int updateByExample(@Param("record") pes_cs_chat_session_service_index_2019 record, @Param("example") pes_cs_chat_session_service_index_2019Example example);
+
+    int updateByPrimaryKeySelective(pes_cs_chat_session_service_index_2019 record);
+
+    int updateByPrimaryKey(pes_cs_chat_session_service_index_2019 record);
+}
