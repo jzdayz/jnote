fce19d287b16a46d424715d540934b2626243cf5
胡晴风 <1397649957@qq.com>
2019-07-15 17:53:08 +0800

    x

---
+++ test/src/main/java/org/zookeeper/TestZookeeper.java
@@ -0,0 +1,8 @@
+package org.zookeeper;
+
+/**
+ * @author ☞ 🏀 huqingfeng
+ * @date 2019-07-15
+ */
+public class TestZookeeper {
+}
--- test/src/test/org/spring/Transaction.java
+++ test/src/test/org/spring/Transaction.java
@@ -1,28 +1,44 @@
 package org.spring;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.junit.Test;
 import org.spring.depen.CallCountingTransactionManager;
 import org.spring.depen.SerializationTestUtils;
 import org.springframework.aop.framework.Advised;
 import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.transaction.annotation.AnnotationTransactionAttributeSource;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 import org.springframework.transaction.interceptor.TransactionInterceptor;
 
+import javax.sql.DataSource;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author ☞ 🏀 huqingfeng
  * @date 2019-07-12
  */
 public class Transaction {
+
+    private static DataSource dataSource;
+
+    static {
+        MysqlDataSource dataSourceMysql = new MysqlDataSource();
+        dataSourceMysql.setUrl("jdbc:mysql://182.254.131.140/pes_jd_sub_01?" +
+                "serverTimezone=Asia/Shanghai&nullCatalogMeansCurrent=true");
+        dataSourceMysql.setUser("wangmeng");
+        dataSourceMysql.setPassword("Ywc201405");
+        dataSource =  dataSourceMysql;
+    }
     @Test
     public void test1() throws Exception{
         TestBean1 tb = new TestBean1();
-        CallCountingTransactionManager ptm = new CallCountingTransactionManager();
+        DataSourceTransactionManager ptm = new DataSourceTransactionManager(dataSource);
         AnnotationTransactionAttributeSource tas = new AnnotationTransactionAttributeSource();
         TransactionInterceptor ti = new TransactionInterceptor(ptm, tas);
 
@@ -32,20 +48,20 @@ public class Transaction {
         proxyFactory.setTarget(tb);
         ITestBean1 proxy = (ITestBean1) proxyFactory.getProxy();
         proxy.getAge();
-        assertThat(ptm.commits).isEqualTo(1);
+//        assertThat(ptm.commits).isEqualTo(1);
 
-        ITestBean1 serializedProxy = (ITestBean1) SerializationTestUtils.serializeAndDeserialize(proxy);
-        serializedProxy.getAge();
-        Advised advised = (Advised) serializedProxy;
-        TransactionInterceptor serializedTi = (TransactionInterceptor) advised.getAdvisors()[0].getAdvice();
-        CallCountingTransactionManager serializedPtm =
-                (CallCountingTransactionManager) serializedTi.getTransactionManager();
-        assertThat(serializedPtm.commits).isEqualTo(2);
+//        ITestBean1 serializedProxy = (ITestBean1) SerializationTestUtils.serializeAndDeserialize(proxy);
+//        serializedProxy.getAge();
+//        Advised advised = (Advised) serializedProxy;
+//        TransactionInterceptor serializedTi = (TransactionInterceptor) advised.getAdvisors()[0].getAdvice();
+//        CallCountingTransactionManager serializedPtm =
+//                (CallCountingTransactionManager) serializedTi.getTransactionManager();
+//        assertThat(serializedPtm.commits).isEqualTo(2);
     }
 
     interface ITestBean1 {
 
-        int getAge();
+        int getAge() throws Exception;
 
         void setAge(int age);
 
@@ -78,10 +94,15 @@ public class Transaction {
             this.name = name;
         }
 
+
+
         @Override
         @Transactional(rollbackFor = Exception.class)
-        public int getAge() {
+        public int getAge()  throws Exception{
             // 手动失败
+            Connection connection = DataSourceUtils.getConnection(dataSource);
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into test(id,name) value (null , '888888')");
+            preparedStatement.execute();
             TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
             return age;
         }
