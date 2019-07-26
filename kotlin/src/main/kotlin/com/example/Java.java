e714d97b15eaad88b841450d26382661d19a29dc
胡晴风 <1397649957@qq.com>
2019-07-26 09:42:36 +0800

    ddddddd

--- kotlin/src/main/kotlin/com/example/Java.java
+++ kotlin/src/main/kotlin/com/example/Java.java
@@ -1,12 +1,31 @@
 package com.example;
 
+import java.lang.reflect.Constructor;
+
 /**
  * @author ☞ 🏀 huqingfeng
  * @date 2019-07-23
  */
 public class Java {
-    public static void main(String[] args) {
+
+
+    public Java(String a){
+
+    }
+
+    public Java(String a,String b){
+
+    }
+
+    public static void main(String[] args) throws Exception{
         Customer customer = new Customer();
         customer.sayHello("哈哈");
+
+        Constructor<?>[] constructors = Java.class.getConstructors();
+        System.out.println(constructors);
+        Constructor<Java> declaredConstructor = Java.class.getDeclaredConstructor();
+        System.out.println(declaredConstructor);
+
+        System.out.println();
     }
 }
